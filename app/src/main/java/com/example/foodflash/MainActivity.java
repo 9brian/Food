746374de7a86093682c5a2cd135d637c3c1ebc62
Brian Palomar <104478722+9brian@users.noreply.github.com>
2023-04-18 12:51:48 -0700

    Create a comment

--- app/src/main/java/com/example/foodflash/MainActivity.java
+++ app/src/main/java/com/example/foodflash/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Adding a comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
