81fc0aaf04420a2792e1e2a1ff6eaa2c688f6d1f
Brian Palomar <104478722+9brian@users.noreply.github.com>
2023-04-18 15:44:38 -0700

    Setup Main Page

--- app/src/main/java/com/example/foodflash/FoodFlash.java
+++
@@ -1,8 +0,0 @@
-package com.example.foodflash;
-
-public class FoodFlash {
-
-    private String mUsername;
-    private double mPassword;
-
-}
--- app/src/main/java/com/example/foodflash/MainActivity.java
+++ app/src/main/java/com/example/foodflash/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.foodflash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     // Adding a comment
+
+    Button mLoginButton;
+    Button mSignUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
