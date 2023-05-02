383395632eefa7e40b4379610e8ab84c249e09b0
Brian Palomar <104478722+9brian@users.noreply.github.com>
2023-05-02 11:48:01 -0700

    Finish Order HistoryActivity

--- app/src/main/java/com/example/foodflash/HistoryActivity.java
+++ app/src/main/java/com/example/foodflash/HistoryActivity.java
@@ -6,10 +6,12 @@ import androidx.room.Room;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.foodflash.DB.AppDataBase;
 import com.example.foodflash.DB.CartDAO;
+import com.example.foodflash.DB.DiscountDAO;
 import com.example.foodflash.DB.ItemDAO;
 import com.example.foodflash.DB.UserDAO;
 import com.example.foodflash.databinding.ActivityHistoryBinding;
@@ -19,14 +21,18 @@ import java.util.List;
 public class HistoryActivity extends AppCompatActivity {
 
     TextView mHistory;
+    TextView mTotal;
 
     UserDAO mUserDAO;
     ItemDAO mItemDAO;
     CartDAO mCartDAO;
+    DiscountDAO mDiscountDAO;
 
     User getUser;
+    Discount getDiscount;
 
     List<Cart> userPurchases;
+    List<Cart> newUserPurchases;
     Item userItems;
 
     ActivityHistoryBinding binding;
@@ -40,6 +46,7 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         mHistory = binding.historyOrderTextview;
+        mTotal = binding.totalOrderTextview;
 
         mUserDAO = Room.databaseBuilder(this, AppDataBase.class, AppDataBase.DATABASE_NAME)
                 .allowMainThreadQueries().build().UserDAO();
@@ -47,6 +54,8 @@ public class HistoryActivity extends AppCompatActivity {
                 .allowMainThreadQueries().build().ItemDAO();
         mCartDAO = Room.databaseBuilder(this, AppDataBase.class, AppDataBase.DATABASE_NAME)
                 .allowMainThreadQueries().build().CartDAO();
+        mDiscountDAO = Room.databaseBuilder(this, AppDataBase.class, AppDataBase.DATABASE_NAME)
+                .allowMainThreadQueries().build().DiscountDAO();
 
 
 //      https://www.geeksforgeeks.org/how-to-send-data-from-one-activity-to-second-activity-in-android/
@@ -56,30 +65,88 @@ public class HistoryActivity extends AppCompatActivity {
 
         getUser = mUserDAO.getUserByName(name);
         int userId = getUser.getUserId();
+        int discountId = getUser.getDiscountId();
 
-        refreshDisplay(userId);
+        Log.d("tag", userId + " " + discountId);
+
+        refreshDisplay(userId, discountId);
+//        refreshTotal(discountId);
     }
 
-    private void refreshDisplay(int userId){
+    private void refreshDisplay(int userId, int discountIdentifier){
         userPurchases = mCartDAO.getItemsByUserId(userId);
+        double subtotal = 0.00;
+
+        Discount discount = mDiscountDAO.getDiscountById(discountIdentifier);
+        double discountPercentage = discount.getDiscountPercentage();
+
         if (! userPurchases.isEmpty()){
             StringBuilder sb = new StringBuilder();
+            StringBuilder stringBuilt = new StringBuilder();
             for(Cart purchases : userPurchases){
-//                sb.append(purchases.toString());
                 int itemId = purchases.getMenuId();
 
-
                 userItems = mItemDAO.getItemsByUserId(itemId);
-                // find the Item
                 sb.append(userItems.getItemName() + "\n$" +
                         userItems.getItemPrice() + "\n" +
                         "=-=-=-=-=-=-=-=-=-=-=" + "\n");
+                subtotal += userItems.getItemPrice();
+                // Fetch discounts
+                // Show subtotal
 
             }
+            Log.d("tag", String.valueOf(subtotal));
+
+            double discountOff = discountPercentage * subtotal;
+            stringBuilt.append("=-=-=-=-=-=-=-=-=--=-=-=-=-=").append("\n");
+            stringBuilt.append("Order Summary").append("\n");
+            stringBuilt.append("Subtotal: $").append(subtotal).append("\n");
+            stringBuilt.append("Discounts: -$").append(discountOff).append("\n");
+
+            double total = 0.00;
+            total = subtotal - discountOff;
+            stringBuilt.append("Order total: $").append(total).append("\n");
+
+            mTotal.setText(stringBuilt);
             mHistory.setText(sb.toString());
         }
         else{
             mHistory.setText("No purchases yet! Time to scrounge some scrilla");
+        }
+    }
+
+    private void refreshTotal(int discountIdentifer){
+        newUserPurchases = mCartDAO.getItemsByUserId(discountIdentifer);
+        Double subtotal = 0.00;
+
+        Discount discount = mDiscountDAO.getDiscountById(discountIdentifer);
+        double discountPercentage = discount.getDiscountPercentage();
+
+
+
+        if (! newUserPurchases.isEmpty()){
+            StringBuilder sb = new StringBuilder();
+            for(Cart purchases : newUserPurchases){
+                int itemId = purchases.getMenuId();
+
+                userItems = mItemDAO.getItemsByUserId(itemId);
+//                subtotal += userItems.getItemPrice();
+                Log.d("tag",userItems.toString());
+                // Fetch discounts
+                // Show subtotal
+
+            }
+//            double discountOff = discountPercentage * subtotal;
+//            sb.append("Subtotal: $").append(subtotal).append("\n");
+//            sb.append("-$").append(discountOff).append("\n");
+//
+//            double total = 0.00;
+//            total = subtotal - discountOff;
+//            sb.append("Total: $").append(total).append("\n");
+//
+//            mTotal.setText(sb);
+//
+////            mHistory.setText(sb.toString());
         }
     }
 
